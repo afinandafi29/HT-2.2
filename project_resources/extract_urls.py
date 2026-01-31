4e92a7cf454690db72fc279fa611f76e8d6bc136
afinandafi29 <afinandafi29@gmail.com>
2026-01-31 18:17:21 +0530

    Delete project_resources directory

--- project_resources/extract_urls.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text = """
-<USER_REQUEST_CONTENT_HERE>
-"""
-
-# The user request content is huge, I'll just use a regex on the provided text in my thought block or use a script if I can pass the content.
-# Actually, I'll just manually pick them or use a more efficient way.
